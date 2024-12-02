91323f8250fda9ef841bbc26450c216e1ed178aa
dilopezch <dilopezch@gmail.com>
2024-12-02 11:27:53 -0500

    fix: corrige preloader

--- js/openpdf.js
+++ js/openpdf.js
@@ -1,13 +1,9 @@
 function openPdfSection(page) {
-  const preloader = document.getElementById('preloader');
-  preloader.classList.add('active');
-  setTimeout(() => {
-    const url = window.location.href;
-    const urlObj = new URL(url);
-    urlObj.pathname = `/pages/pdf_viewer.html`;
-    urlObj.searchParams.append("page", page);
-    window.location.href = urlObj.toString();
-  }, 1000);
+  const url = window.location.href;
+  const urlObj = new URL(url);
+  urlObj.pathname = `/pages/pdf_viewer.html`;
+  urlObj.searchParams.append("page", page);
+  window.location.href = urlObj.toString();
 }
 
 const url = window.location.href;
